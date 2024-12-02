50801ba7e343d1b7dd7ecc301b0f438c0daf89b3
anuradha34 <anu2017ray@gmail.com>
2024-12-02 18:58:11 +0530

    Initial commit

---
+++ scrimbareact/vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Samp from "./sampv1.jsx";
+function App() {
+ return (
+     <>
+         <h2>Welcome to sample</h2>
+         <Samp/>
+         <h4>This is the end</h4>
+     </>
+
+ )
+}
+
+export default App
---
+++ scrimbareact/vitereact/src/sampv1.jsx
@@ -0,0 +1,6 @@
+function Samp(){
+    return (
+        <h3>sample is ready</h3>
+    )
+}
+export default Samp
